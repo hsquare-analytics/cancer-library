e923f780d2ae112d740de45ba8dba6b1ba1408a5
zero <zero@planit.ai>
2022-09-02 13:22:40 +0900

    style: toast position

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -38,7 +38,7 @@ export const App = () => {
   return (
     <BrowserRouter basename={baseHref}>
       <div className="app-container" style={{ paddingTop, paddingLeft }}>
-        <ToastContainer position={toast.POSITION.TOP_LEFT} className="toastify-container" toastClassName="toastify-toast" />
+        <ToastContainer position={toast.POSITION.TOP_CENTER} className="toastify-container" toastClassName="toastify-toast" />
         <ErrorBoundary>
           <Header
             isAuthenticated={isAuthenticated}
