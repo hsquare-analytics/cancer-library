e39ec506d06b703923a65263175bbb25be3cfe0b
Eliars <eliars@planit.ai>
2022-09-05 19:21:19 +0900

    기존 UI 삭제 일부 UI 업데이트

--- src/main/webapp/app/entities/point/index.tsx
+++ src/main/webapp/app/entities/point/index.tsx
@@ -5,16 +5,13 @@ import ErrorBoundaryRoutes from 'app/shared/error/error-boundary-routes';
 
 import Point from './point';
 import PointDetail from './point-detail';
-import PointUpdate from './point-update';
 import PointDeleteDialog from './point-delete-dialog';
 
 const PointRoutes = () => (
   <ErrorBoundaryRoutes>
     <Route index element={<Point />} />
-    <Route path="new" element={<PointUpdate />} />
     <Route path=":id">
       <Route index element={<PointDetail />} />
-      <Route path="edit" element={<PointUpdate />} />
       <Route path="delete" element={<PointDeleteDialog />} />
     </Route>
   </ErrorBoundaryRoutes>
--- src/main/webapp/app/entities/point/point.tsx
+++ src/main/webapp/app/entities/point/point.tsx
@@ -1,27 +1,42 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect, useRef } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { Button, Table } from 'reactstrap';
-import { Translate, getSortState, JhiPagination, JhiItemCount } from 'react-jhipster';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Translate, getSortState } from 'react-jhipster';
 
 import { ASC, DESC, ITEMS_PER_PAGE, SORT } from 'app/shared/util/pagination.constants';
 import { overridePaginationStateWithQueryParams } from 'app/shared/util/entity-utils';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 
+import { IPoint } from "app/shared/model/point.model";
+
 // Dev Extreme
-import { DataGrid,  Form, Editing, Paging, Popup } from 'devextreme-react/data-grid';
-import { Item } from 'devextreme-react/form';
+import { DataGrid,
+  Form,
+  Editing,
+  Paging,
+  Popup,
+  Toolbar,
+  Item as ItemToolbar,
+  Column,
+  SearchPanel,
+  FilterRow,
+  LoadPanel,
+} from 'devextreme-react/data-grid';
+import { Item as ItemFrom }  from 'devextreme-react/form';
+import { Button as ButtonDevExtreme } from 'devextreme-react/button';
 
 // Dev Extreme CSS
 import 'devextreme/dist/css/dx.material.lime.light.compact.css'
 
-import { getEntities, createEntity, deleteEntity, updateEntity } from './point.reducer';
+import { getEntities, createEntity, updateEntity } from './point.reducer';
 
 import { cloneDeep } from 'lodash'
-import { IPoint } from "app/shared/model/point.model";
+
+// Page SCSS
+import './point.scss'
 
 export const Point = () => {
   const dispatch = useAppDispatch();
+  const popupRef = useRef(null);
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -31,16 +46,13 @@ export const Point = () => {
   );
 
   const pointList = useAppSelector(state => state.point.entities);
-  const loading = useAppSelector(state => state.point.loading);
-  const totalItems = useAppSelector(state => state.point.totalItems);
+  const errorMessage = useAppSelector(state => state.point.errorMessage);
 
   const copyItem = (origin) => {
     return cloneDeep(origin);
   }
 
   const [pointListCopy, setPointListCopy] = useState(copyItem(pointList))
-
-  const [isEditing, setIsEditing] = useState(false)
 
   const getAllEntities = () => {
     dispatch(
@@ -52,22 +64,17 @@ export const Point = () => {
     );
   };
 
+  const onInitNewRow = (e) => {
+    e.component.option('editing.popup.title', 'new data')
+  }
+
+  const onEditingStart = (e) => {
+    e.component.option('editing.popup.title', e.data.id)
+  }
+
   const onRowInsert = (e) => {
     const data : IPoint = e.data;
-    dispatch(createEntity(data))
-  }
-
-  const onRowDelete = (e) => {
-    const data : IPoint = e.data;
-    dispatch(deleteEntity(data.id))
-  }
-
-  const onStartEditing = (e) => {
-    setIsEditing(true);
-  }
-
-  const onEditCanceled = (e) => {
-    setIsEditing(false);
+    dispatch(createEntity(data));
   }
 
   const getNewData = (object, property) => {
@@ -81,7 +88,6 @@ export const Point = () => {
     data.description = getNewData(e, 'description')
 
     dispatch(updateEntity(data))
-    setIsEditing(false);
   }
 
   const sortEntities = () => {
@@ -91,6 +97,10 @@ export const Point = () => {
       navigate(`${location.pathname}${endURL}`);
     }
   };
+
+  useEffect(() => {
+    console.log(errorMessage)
+  }, [errorMessage]);
 
   useEffect(() => {
     sortEntities();
@@ -116,20 +126,6 @@ export const Point = () => {
     setPointListCopy(temp)
   }, [pointList])
 
-  const sort = p => () => {
-    setPaginationState({
-      ...paginationState,
-      order: paginationState.order === ASC ? DESC : ASC,
-      sort: p,
-    });
-  };
-
-  const handlePagination = currentPage =>
-    setPaginationState({
-      ...paginationState,
-      activePage: currentPage,
-    });
-
   const handleSyncList = () => {
     sortEntities();
   };
@@ -139,150 +135,60 @@ export const Point = () => {
       <DataGrid
         dataSource={pointListCopy}
         keyExpr='id'
-        defaultColumns={['id', 'title', 'description']}
         showBorders={true}
+        showColumnLines={true}
+        onEditingStart={onEditingStart}
+        onInitNewRow={onInitNewRow}
         onRowInserting={onRowInsert}
         onRowUpdating={onRowUpdate}
-        onRowRemoved={onRowDelete}
-        onEditingStart={onStartEditing}
-        onEditCanceled={onEditCanceled}
       >
-        <Paging enabled={false} />
+        <Toolbar>
+          <ItemToolbar location="before">
+            <div className='informer'>
+              <h2 className='count'>{pointList.length}</h2>
+              <span>Total Count</span>
+            </div>
+          </ItemToolbar>
+          <ItemToolbar location="after">
+            <ButtonDevExtreme
+              icon='refresh'
+              onClick={handleSyncList}
+            />
+          </ItemToolbar>
+          <ItemToolbar name='addRowButton' />
+          <ItemToolbar name='applyFilterButton' />
+        </Toolbar>
+        <Paging enabled={true} pageSize={15} defaultPageSize={15}/>
+        <SearchPanel visible={true} />
+        <FilterRow visible={true} />
+        <LoadPanel enabled={true} />
         <Editing
           mode="popup"
           allowUpdating={true}
           allowAdding={true}
-          allowDeleting={true}
+          allowDeleting={false}
         >
           <Popup
-            title={'Point DevExtrem Test'}
             showTitle={true}
             width={700}
             height={525}
+            hideOnOutsideClick={true}
           />
-
           <Form>
-            <Item
+            <ItemFrom
               itemType="group"
               colCount={2}
               colSpan={2}
             >
-              {
-                isEditing && (
-                  <Item dataField={"id"} disabled/>
-                )
-              }
-              <Item dataField={"title"} />
-              <Item dataField={"description"}/>
-            </Item>
+              <ItemFrom dataField={"title"} />
+              <ItemFrom dataField={"description"}/>
+            </ItemFrom>
           </Form>
         </Editing>
+        <Column dataField={'id'} alignment={'left'} />
+        <Column dataField={'title'} alignment='center' caption='title' />
+        <Column dataField={'description'} alignment='center' caption='description' />
       </DataGrid>
-      <h2 id="point-heading" data-cy="PointHeading">
-        <Translate contentKey="cancerLibraryApp.point.home.title">Points</Translate>
-        <div className="d-flex justify-content-end">
-          <Button className="me-2" color="info" onClick={handleSyncList} disabled={loading}>
-            <FontAwesomeIcon icon="sync" spin={loading} />{' '}
-            <Translate contentKey="cancerLibraryApp.point.home.refreshListLabel">Refresh List</Translate>
-          </Button>
-          <Link to="/point/new" className="btn btn-primary jh-create-entity" id="jh-create-entity" data-cy="entityCreateButton">
-            <FontAwesomeIcon icon="plus" />
-            &nbsp;
-            <Translate contentKey="cancerLibraryApp.point.home.createLabel">Create new Point</Translate>
-          </Link>
-        </div>
-      </h2>
-      <div className="table-responsive">
-        {pointList && pointList.length > 0 ? (
-          <Table responsive>
-            <thead>
-              <tr>
-                <th className="hand" onClick={sort('id')}>
-                  <Translate contentKey="cancerLibraryApp.point.id">ID</Translate> <FontAwesomeIcon icon="sort" />
-                </th>
-                <th className="hand" onClick={sort('title')}>
-                  <Translate contentKey="cancerLibraryApp.point.title">Title</Translate> <FontAwesomeIcon icon="sort" />
-                </th>
-                <th className="hand" onClick={sort('description')}>
-                  <Translate contentKey="cancerLibraryApp.point.description">Description</Translate> <FontAwesomeIcon icon="sort" />
-                </th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {pointList.map((point, i) => (
-                <tr key={`entity-${i}`} data-cy="entityTable">
-                  <td>
-                    <Button tag={Link} to={`/point/${point.id}`} color="link" size="sm">
-                      {point.id}
-                    </Button>
-                  </td>
-                  <td>{point.title}</td>
-                  <td>{point.description}</td>
-                  <td className="text-end">
-                    <div className="btn-group flex-btn-group-container">
-                      <Button tag={Link} to={`/point/${point.id}`} color="info" size="sm" data-cy="entityDetailsButton">
-                        <FontAwesomeIcon icon="eye" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.view">View</Translate>
-                        </span>
-                      </Button>
-                      <Button
-                        tag={Link}
-                        to={`/point/${point.id}/edit?page=${paginationState.activePage}&sort=${paginationState.sort},${paginationState.order}`}
-                        color="primary"
-                        size="sm"
-                        data-cy="entityEditButton"
-                      >
-                        <FontAwesomeIcon icon="pencil-alt" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.edit">Edit</Translate>
-                        </span>
-                      </Button>
-                      <Button
-                        tag={Link}
-                        to={`/point/${point.id}/delete?page=${paginationState.activePage}&sort=${paginationState.sort},${paginationState.order}`}
-                        color="danger"
-                        size="sm"
-                        data-cy="entityDeleteButton"
-                      >
-                        <FontAwesomeIcon icon="trash" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.delete">Delete</Translate>
-                        </span>
-                      </Button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        ) : (
-          !loading && (
-            <div className="alert alert-warning">
-              <Translate contentKey="cancerLibraryApp.point.home.notFound">No Points found</Translate>
-            </div>
-          )
-        )}
-      </div>
-      {totalItems ? (
-        <div className={pointList && pointList.length > 0 ? '' : 'd-none'}>
-          <div className="justify-content-center d-flex">
-            <JhiItemCount page={paginationState.activePage} total={totalItems} itemsPerPage={paginationState.itemsPerPage} i18nEnabled />
-          </div>
-          <div className="justify-content-center d-flex">
-            <JhiPagination
-              activePage={paginationState.activePage}
-              onSelect={handlePagination}
-              maxButtons={5}
-              itemsPerPage={paginationState.itemsPerPage}
-              totalItems={totalItems}
-            />
-          </div>
-        </div>
-      ) : (
-        ''
-      )}
     </div>
   );
 };
