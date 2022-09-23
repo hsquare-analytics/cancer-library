958fd155095f72a22145e4cf0531091db7371dcd
zero <zero@planit.ai>
2022-09-23 09:56:22 +0900

    feat: DatasourceApproval

---
+++ src/test/java/io/planit/cancerlibrary/web/rest/DatasourceApprovalControllerIT.java
@@ -0,0 +1,79 @@
+package io.planit.cancerlibrary.web.rest;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import io.planit.cancerlibrary.IntegrationTest;
+import io.planit.cancerlibrary.domain.Category;
+import io.planit.cancerlibrary.domain.Subject;
+import io.planit.cancerlibrary.domain.Topic;
+import io.planit.cancerlibrary.mapper.DatasourceMapper;
+import io.planit.cancerlibrary.mapper.SQLAdapter;
+import io.planit.cancerlibrary.repository.CategoryRepository;
+import io.planit.cancerlibrary.repository.SubjectRepository;
+import io.planit.cancerlibrary.repository.TopicRepository;
+import io.planit.cancerlibrary.security.AuthoritiesConstants;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@IntegrationTest
+@AutoConfigureMockMvc
+@WithMockUser(authorities = AuthoritiesConstants.ADMIN)
+public class DatasourceApprovalControllerIT {
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private MockMvc restAdminDatasourceMockMvc;
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    private Category category;
+
+    @BeforeEach
+    void initTest() {
+        Subject subject = SubjectResourceIT.createEntity(em);
+        subjectRepository.saveAndFlush(subject);
+
+        Topic topic = TopicResourceIT.createEntity(em, subject);
+        topicRepository.saveAndFlush(topic);
+
+        category = CategoryResourceIT.createEntity(em, topic).title("test_member");
+        categoryRepository.saveAndFlush(category);
+    }
+
+    @Autowired
+    private DatasourceMapper datasourceMapper;
+
+    @Test
+    @Transactional
+    public void testFetchDatasourceByCategoryId() throws Exception {
+        datasourceMapper.executeInsertSQL(
+            new SQLAdapter("INSERT INTO TEST_MEMBER_UPDATED (IDX, NAME) VALUES (10001, 'ZERO')"));
+
+        restAdminDatasourceMockMvc
+            .perform(get("/api/datasource-approval/{id}", category.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[*].idx").value(hasItem(10001)))
+            .andExpect(jsonPath("$.[*].name").value(hasItem("ZERO")));
+    }
+}
