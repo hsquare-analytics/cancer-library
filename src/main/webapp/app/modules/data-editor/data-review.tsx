7b09733978eb6ebcc8021dca184414f9e1b471a9
zero <zero@planit.ai>
2022-09-26 15:26:13 +0900

    modify: data-review.tsx - i18n info

--- src/main/webapp/app/modules/data-editor/data-review.tsx
+++ src/main/webapp/app/modules/data-editor/data-review.tsx
@@ -13,6 +13,7 @@ import axios from "axios";
 import {toast} from 'react-toastify';
 import {APP_DX_DATE_FORMAT, AUTHORITIES, REVIEW_LIST} from "app/config/constants";
 import {hasAnyAuthority} from "app/shared/auth/private-route";
+import {translate} from "react-jhipster";
 
 export const DataReview = () => {
   const dispatch = useAppDispatch();
@@ -114,11 +115,11 @@ export const DataReview = () => {
             />
           )
         }
-        <Column caption="생성인" dataField={"created_by"} alignment={"center"} minWidth={150} allowEditing={false}/>
-        <Column caption="생성일" dataField={"created_date"} dataType="date" format={APP_DX_DATE_FORMAT} alignment={"center"} minWidth={150} allowEditing={false}/>
-        <Column caption="최종 수정인" dataField={"last_modified_by"} alignment={"center"} minWidth={150} allowEditing={false}/>
-        <Column caption="취종 수정일" dataField={"last_modified_date"} dataType="date" format={APP_DX_DATE_FORMAT} alignment={"center"} minWidth={150} allowEditing={false}/>
-        <Column caption={"상태"} dataField={"status"} alignment={'center'} minWidth={150} allowEditing={true}>
+        <Column caption={translate("datasource.column.createdBy")} dataField={"created_by"} alignment={"center"} minWidth={150} allowEditing={false}/>
+        <Column caption={translate("datasource.column.createdDate")} dataField={"created_date"} dataType="date" format={APP_DX_DATE_FORMAT} alignment={"center"} minWidth={150} allowEditing={false}/>
+        <Column caption={translate("datasource.column.lastModifiedBy")} dataField={"last_modified_by"} alignment={"center"} minWidth={150} allowEditing={false}/>
+        <Column caption={translate("datasource.column.lastModifiedDate")}  dataField={"last_modified_date"} dataType="date" format={APP_DX_DATE_FORMAT} alignment={"center"} minWidth={150} allowEditing={false}/>
+        <Column caption={translate("datasource.column.status")} dataField={"status"} alignment={'center'} minWidth={150} allowEditing={true}>
           <Lookup dataSource={Object.values(REVIEW_LIST)}/>
         </Column>
       </DataGrid>
