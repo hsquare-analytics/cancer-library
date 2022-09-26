0cba1750a8649f2a25da226ce9538d52fc975697
zero <zero@planit.ai>
2022-09-26 13:25:57 +0900

    refactor: Mybatis, Jpa - datasource config dualize

--- src/main/java/io/planit/cancerlibrary/config/JpaConfiguration.java
+++ src/main/java/io/planit/cancerlibrary/config/JpaConfiguration.java
@@ -1,11 +1,15 @@
 package io.planit.cancerlibrary.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.util.Objects;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -22,6 +26,20 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 )
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 public class JpaConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.jpa")
+    public HikariConfig jpaHikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    @Primary
+    public DataSource jpaDatasource() throws Exception {
+        DataSource dataSource = new HikariDataSource(jpaHikariConfig());
+        return dataSource;
+    }
+
     @Bean
     public LocalContainerEntityManagerFactoryBean jpaEntityManagerFactory(EntityManagerFactoryBuilder builder, @Qualifier("jpaDatasource") DataSource dataSource) {
         return builder
--- src/main/java/io/planit/cancerlibrary/config/MybatisConfiguration.java
+++ src/main/java/io/planit/cancerlibrary/config/MybatisConfiguration.java
@@ -1,22 +1,32 @@
 package io.planit.cancerlibrary.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 @MapperScan(basePackages = "io.planit.cancerlibrary.mapper")
-public class MybatisDatabaseConfig {
-    @Autowired
-    private ApplicationContext applicationContext;
+public class MybatisConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.mybatis")
+    public HikariConfig mybatisHikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource mybatisDatasource() throws Exception {
+        DataSource dataSource = new HikariDataSource(mybatisHikariConfig());
+        return dataSource;
+    }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(@Qualifier("mybatisDatasource") DataSource dataSource) throws Exception {
