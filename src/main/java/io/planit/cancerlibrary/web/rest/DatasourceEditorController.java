5ce6448d13612f824c9cb559ab2a5f09299afa14
zero <zero@planit.ai>
2022-10-21 15:40:03 +0900

    feat: DatasourceEditorController - create row method

--- src/main/java/io/planit/cancerlibrary/web/rest/DatasourceEditorController.java
+++ src/main/java/io/planit/cancerlibrary/web/rest/DatasourceEditorController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,18 @@ public class DatasourceEditorController {
         this.unionSqlBuilderService = unionSqlBuilderService;
         this.datasourceMapper = datasourceMapper;
         this.dmlSqlBuilderService = dmlSqlBuilderService;
+    }
+
+    @PostMapping("/datasource-editor/categories/{categoryId}")
+    public ResponseEntity<Integer> createDatasourceRow(@PathVariable Long categoryId, @RequestBody Map<String, String> map) {
+        log.debug("Request to create datasource row by categoryId: {}", categoryId);
+
+        Map mapWithIdx = new HashMap<String, Object>(map);
+        mapWithIdx.put("idx", "10001");
+
+        SQL insertSQL = dmlSqlBuilderService.getInsertSQL(categoryId, mapWithIdx);
+        Integer result = datasourceMapper.executeInsertSQL(new SQLAdapter(insertSQL));
+        return ResponseEntity.ok().body(result);
     }
 
     @GetMapping("/datasource-editor/categories/{categoryId}")
