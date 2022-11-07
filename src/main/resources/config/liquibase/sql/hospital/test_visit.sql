361fc1b208e603ed295c38741c5ac8c3d405d850
zero <zero@planit.ai>
2022-11-07 15:17:40 +0900

    chore: liquibase/test_codebool sql

--- src/main/resources/config/liquibase/sql/hospital/test_visit.sql
+++ src/main/resources/config/liquibase/sql/hospital/test_visit.sql
@@ -39,13 +39,11 @@ values (2703, 'PT_NM', true, 1, 2701, 'system', '2019-01-01 00:00:00', 'system',
 insert into ph_item_property (item_id, visible_index, caption, allow_editing) values (2703, 3, '환자 이름', false);
 insert into ph_item_attribute (item_id, data_type) values (2703, 'string');
 
-insert into ph_item (id, title, activated, order_no, category_id, created_by, created_date, last_modified_by,
+insert into ph_item (id, title, activated, order_no, category_id, codebook_id, created_by, created_date, last_modified_by,
                      last_modified_date)
-values (2704, 'SEX_TP_CD', true, 1, 2701, 'system', '2019-01-01 00:00:00', 'system', '2019-01-01 00:00:00');
+values (2704, 'SEX_TP_CD', true, 1, 2701, 101, 'system', '2019-01-01 00:00:00', 'system', '2019-01-01 00:00:00');
 insert into ph_item_property (item_id, visible_index, caption, allow_editing) values (2704, 4, '성별', true);
 insert into ph_item_attribute (item_id, data_type) values (2704, 'selectbox');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2704, 1, 'M', 'M');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2704, 2, 'F', 'F');
 
 insert into ph_item (id, title, activated, order_no, category_id, created_by, created_date, last_modified_by,
                      last_modified_date)
@@ -53,14 +51,11 @@ values (2705, 'PT_BRDY_DT', true, 1, 2701, 'system', '2019-01-01 00:00:00', 'sys
 insert into ph_item_property (item_id, visible_index, caption, allow_editing, format) values (2705, 5, '생년월일', true, 'yyyy/MM/dd');
 insert into ph_item_attribute (item_id, data_type) values (2705, 'date');
 
-insert into ph_item (id, title, activated, order_no, category_id, created_by, created_date, last_modified_by,
+insert into ph_item (id, title, activated, order_no, category_id, codebook_id, created_by, created_date, last_modified_by,
                      last_modified_date)
-values (2706, 'HSP_TP_CD', true, 1, 2701, 'system', '2019-01-01 00:00:00', 'system', '2019-01-01 00:00:00');
+values (2706, 'HSP_TP_CD', true, 1, 2701, 102, 'system', '2019-01-01 00:00:00', 'system', '2019-01-01 00:00:00');
 insert into ph_item_property (item_id, visible_index, caption, allow_editing) values (2706, 6, '병원코드', true);
 insert into ph_item_attribute (item_id, data_type) values (2706, 'selectbox');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2706, 1, '01', '01');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2706, 2, '02', '02');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2706, 3, '03', '03');
 
 insert into ph_item (id, title, activated, order_no, category_id, created_by, created_date, last_modified_by,
                      last_modified_date)
@@ -74,15 +69,11 @@ values (2708, 'MED_DT', true, 1, 2701, 'system', '2019-01-01 00:00:00', 'system'
 insert into ph_item_property (item_id, visible_index, caption, allow_editing, format) values (2708, 8, '수진 기록일시', true, 'yyyy/MM/dd HH:mm:ss');
 insert into ph_item_attribute (item_id, data_type) values (2708, 'date');
 
-insert into ph_item (id, title, activated, order_no, category_id, created_by, created_date, last_modified_by,
+insert into ph_item (id, title, activated, order_no, category_id, codebook_id, created_by, created_date, last_modified_by,
                      last_modified_date)
-values (2709, 'HSP_DTL_TP_NM', true, 1, 2701, 'system', '2019-01-01 00:00:00', 'system', '2019-01-01 00:00:00');
+values (2709, 'HSP_DTL_TP_NM', true, 1, 2701, 103, 'system', '2019-01-01 00:00:00', 'system', '2019-01-01 00:00:00');
 insert into ph_item_property (item_id, visible_index, caption, allow_editing) values (2709, 9, '환자 세부 구분', true);
 insert into ph_item_attribute (item_id, data_type) values (2709, 'selectbox');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2709, 1, '어린이', '1');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2709, 2, '청소년', '2');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2709, 3, '어른', '3');
-insert into ph_item_lookup (item_id, order_no, title, description) values (2709, 4, '노인', '4');
 
 insert into ph_item (id, title, activated, order_no, category_id, created_by, created_date, last_modified_by,
                      last_modified_date)
