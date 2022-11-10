815eb5412ae63f839ad05dffe17e48a191751fa3
zero <zero@planit.ai>
2022-11-11 08:54:54 +0900

    chore: liquibase

--- src/main/resources/config/liquibase/sql/hospital/test_patient_authority.sql
+++ src/main/resources/config/liquibase/sql/hospital/test_patient_authority.sql
@@ -1,9 +1,9 @@
-insert into ph_user_patient (id, user_id, patient_no, created_by) values (101, 4, 'G010323613895', 'system');
-insert into ph_user_patient (id, user_id, patient_no, created_by) values (102, 4, 'G010489020065', 'system');
-insert into ph_user_patient (id, user_id, patient_no, created_by) values (103, 4, 'G010483166922', 'system');
-insert into ph_user_patient (id, user_id, patient_no, created_by) values (104, 5, 'G010239806985', 'system');
-insert into ph_user_patient (id, user_id, patient_no, created_by) values (105, 5, 'G010489092240', 'system');
-insert into ph_user_patient (id, user_id, patient_no, created_by) values (106, 5, 'G010333051941', 'system');
-insert into ph_user_patient (id, user_id, patient_no, created_by) values (107, 6, 'G010325146055', 'system');
-insert into ph_user_patient (id, user_id, patient_no, created_by) values (108, 6, 'G010489324436', 'system');
+insert into ph_user_patient (id, user_id, patient_no, created_by) values (101, 4, '41007033', 'system');
+insert into ph_user_patient (id, user_id, patient_no, created_by) values (102, 4, '50511048', 'system');
+insert into ph_user_patient (id, user_id, patient_no, created_by) values (103, 4, '52388073', 'system');
+insert into ph_user_patient (id, user_id, patient_no, created_by) values (104, 5, '41599176', 'system');
+insert into ph_user_patient (id, user_id, patient_no, created_by) values (105, 5, '16621774', 'system');
+insert into ph_user_patient (id, user_id, patient_no, created_by) values (106, 5, '33880073', 'system');
+insert into ph_user_patient (id, user_id, patient_no, created_by) values (107, 6, '31880611', 'system');
+insert into ph_user_patient (id, user_id, patient_no, created_by) values (108, 6, '53215109', 'system');
 
