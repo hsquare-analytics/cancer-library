8e31adffa1d6eb9da3d8962d50c0d0ebf441f88a
zero <zero@planit.ai>
2022-11-10 13:20:04 +0900

    feat: codebook-update.tsx - weave with lookup editor

--- src/main/webapp/app/entities/codebook/codebook-update.tsx
+++ src/main/webapp/app/entities/codebook/codebook-update.tsx
@@ -1,23 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, useNavigate, useParams} from 'react-router-dom';
 import {Button, Col, Row} from 'reactstrap';
 import {Translate, translate, ValidatedField, ValidatedForm} from 'react-jhipster';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {useAppDispatch, useAppSelector} from 'app/config/store';
 import {createEntity, getEntity, reset, updateEntity} from './codebook.reducer';
+import MuiButton from '@mui/material/Button';
+import LookupEditor from "app/modules/datasource-editor/multi-table-editor/lookup-editor/lookup-editor";
+
 
 export const CodebookUpdate = () => {
   const dispatch = useAppDispatch();
 
   const navigate = useNavigate();
 
-  const { id } = useParams<'id'>();
+  const {id} = useParams<'id'>();
   const isNew = id === undefined;
 
   const codebookEntity = useAppSelector(state => state.codebook.entity);
   const loading = useAppSelector(state => state.codebook.loading);
   const updating = useAppSelector(state => state.codebook.updating);
   const updateSuccess = useAppSelector(state => state.codebook.updateSuccess);
+
+  const [showLookup, setShowLookup] = useState(false);
 
   const handleClose = () => {
     navigate('/admin/codebook' + location.search);
@@ -54,15 +59,16 @@ export const CodebookUpdate = () => {
     isNew
       ? {}
       : {
-          ...codebookEntity,
-        };
+        ...codebookEntity,
+      };
 
   return (
     <div>
       <Row className="justify-content-center">
         <Col md="8">
           <h2 id="cancerLibraryApp.codebook.home.createOrEditLabel" data-cy="CodebookCreateUpdateHeading">
-            <Translate contentKey="cancerLibraryApp.codebook.home.createOrEditLabel">Create or edit a Codebook</Translate>
+            <Translate contentKey="cancerLibraryApp.codebook.home.createOrEditLabel">Create or edit a
+              Codebook</Translate>
           </h2>
         </Col>
       </Row>
@@ -79,7 +85,7 @@ export const CodebookUpdate = () => {
                   readOnly
                   id="codebook-id"
                   label={translate('global.field.id')}
-                  validate={{ required: true }}
+                  validate={{required: true}}
                 />
               ) : null}
               <ValidatedField
@@ -89,9 +95,9 @@ export const CodebookUpdate = () => {
                 data-cy="title"
                 type="text"
                 validate={{
-                  required: { value: true, message: translate('entity.validation.required') },
-                  minLength: { value: 1, message: translate('entity.validation.minlength', { min: 1 }) },
-                  maxLength: { value: 50, message: translate('entity.validation.maxlength', { max: 50 }) },
+                  required: {value: true, message: translate('entity.validation.required')},
+                  minLength: {value: 1, message: translate('entity.validation.minlength', {min: 1})},
+                  maxLength: {value: 50, message: translate('entity.validation.maxlength', {max: 50})},
                 }}
               />
               <ValidatedField
@@ -101,30 +107,46 @@ export const CodebookUpdate = () => {
                 data-cy="description"
                 type="text"
               />
-              <ValidatedField
-                label={translate('cancerLibraryApp.codebook.lookupList')}
-                id="codebook-lookupList"
-                name="lookupList"
-                data-cy="lookupList"
-                type="select"
-                multiple
-              >
-                {codebookEntity.lookupList?.filter(data=>data).map(lookup => (
-                  <option value={lookup} key={lookup}>
-                    {lookup.title}
-                  </option>
-                ))}
-              </ValidatedField>
-              <Button tag={Link} id="cancel-save" data-cy="entityCreateCancelButton" to="/admin/codebook" replace color="info">
-                <FontAwesomeIcon icon="arrow-left" />
+              <div>
+                {translate('cancerLibraryApp.codebook.lookupList')}
+                <MuiButton variant="text" onClick={() => setShowLookup(true)}>
+                  <FontAwesomeIcon icon="pencil-alt"/>{' '}
+                </MuiButton>
+                {codebookEntity.lookupList && codebookEntity.lookupList.length > 0 ?
+                  <LookupEditor visible={showLookup} setVisible={setShowLookup}
+                                dataSource={codebookEntity.lookupList.filter(data => data).map(data => {
+                                  return {
+                                    codebookId: codebookEntity.id,
+                                    title: data.title,
+                                    description: data.description,
+                                  }
+                                })}/> : null}
+                <ValidatedField
+                  id="codebook-lookupList"
+                  name="lookupList"
+                  data-cy="lookupList"
+                  type="select"
+                  multiple
+                >
+                  {codebookEntity.lookupList?.filter(data => data).map(lookup => (
+                    <option value={lookup} key={lookup}>
+                      {lookup.title}
+                    </option>
+                  ))}
+                </ValidatedField>
+              </div>
+              <Button tag={Link} id="cancel-save" data-cy="entityCreateCancelButton" to="/admin/codebook" replace
+                      color="info">
+                <FontAwesomeIcon icon="arrow-left"/>
                 &nbsp;
                 <span className="d-none d-md-inline">
                   <Translate contentKey="entity.action.back">Back</Translate>
                 </span>
               </Button>
               &nbsp;
-              <Button color="primary" id="save-entity" data-cy="entityCreateSaveButton" type="submit" disabled={updating}>
-                <FontAwesomeIcon icon="save" />
+              <Button color="primary" id="save-entity" data-cy="entityCreateSaveButton" type="submit"
+                      disabled={updating}>
+                <FontAwesomeIcon icon="save"/>
                 &nbsp;
                 <Translate contentKey="entity.action.save">Save</Translate>
               </Button>
