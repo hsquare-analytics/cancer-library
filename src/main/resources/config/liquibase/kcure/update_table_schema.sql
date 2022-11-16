04e7172edfd7e9baea8d66cfd007c8ed2130a470
zero <zero@planit.ai>
2022-11-16 16:36:06 +0900

    chore

--- src/main/resources/config/liquibase/kcure/update_table_schema.sql
+++ src/main/resources/config/liquibase/kcure/update_table_schema.sql
@@ -1,71 +1,66 @@
--- 00
+ --- cncr_rgst
 DROP TABLE IF EXISTS gstr.cncr_rgst_updated;
 
-CREATE TABLE gstr.cncr_rgst_updated AS
-    TABLE gstr.cncr_rgst
-    WITH NO DATA;
+CREATE TABLE gstr.cncr_rgst_updated AS TABLE gstr.cncr_rgst WITH NO DATA;
 
 ALTER TABLE gstr.cncr_rgst_updated  ADD COLUMN CREATED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.cncr_rgst_updated  ADD COLUMN CREATED_DATE TIMESTAMP;
 ALTER TABLE gstr.cncr_rgst_updated  ADD COLUMN LAST_MODIFIED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.cncr_rgst_updated  ADD COLUMN LAST_MODIFIED_DATE TIMESTAMP;
+---
 
--- 01
+ --- gstr_pt_hlinf
 DROP TABLE IF EXISTS gstr.gstr_pt_hlinf_updated;
 
-CREATE TABLE gstr.gstr_pt_hlinf_updated AS
-    TABLE gstr.gstr_pt_hlinf
-    WITH NO DATA;
+CREATE TABLE gstr.gstr_pt_hlinf_updated AS TABLE gstr.gstr_pt_hlinf WITH NO DATA;
 
 ALTER TABLE gstr.gstr_pt_hlinf_updated  ADD COLUMN CREATED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_pt_hlinf_updated  ADD COLUMN CREATED_DATE TIMESTAMP;
 ALTER TABLE gstr.gstr_pt_hlinf_updated  ADD COLUMN LAST_MODIFIED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_pt_hlinf_updated  ADD COLUMN LAST_MODIFIED_DATE TIMESTAMP;
+---
 
--- 02
+ --- gstr_pt_fmht
 DROP TABLE IF EXISTS gstr.gstr_pt_fmht_updated;
 
-CREATE TABLE gstr.gstr_pt_fmht_updated AS
-    TABLE gstr.gstr_pt_fmht
-    WITH NO DATA;
+CREATE TABLE gstr.gstr_pt_fmht_updated AS TABLE gstr.gstr_pt_fmht WITH NO DATA;
 
 ALTER TABLE gstr.gstr_pt_fmht_updated  ADD COLUMN CREATED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_pt_fmht_updated  ADD COLUMN CREATED_DATE TIMESTAMP;
 ALTER TABLE gstr.gstr_pt_fmht_updated  ADD COLUMN LAST_MODIFIED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_pt_fmht_updated  ADD COLUMN LAST_MODIFIED_DATE TIMESTAMP;
+---
 
--- 03
+ --- gstr_diag_aninf
 DROP TABLE IF EXISTS gstr.gstr_diag_aninf_updated;
 
-CREATE TABLE gstr.gstr_diag_aninf_updated AS
-    TABLE gstr.gstr_diag_aninf
-    WITH NO DATA;
+CREATE TABLE gstr.gstr_diag_aninf_updated AS TABLE gstr.gstr_diag_aninf WITH NO DATA;
 
 ALTER TABLE gstr.gstr_diag_aninf_updated  ADD COLUMN CREATED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_diag_aninf_updated  ADD COLUMN CREATED_DATE TIMESTAMP;
 ALTER TABLE gstr.gstr_diag_aninf_updated  ADD COLUMN LAST_MODIFIED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_diag_aninf_updated  ADD COLUMN LAST_MODIFIED_DATE TIMESTAMP;
+---
 
--- 04
+ --- gstr_diag_inf
 DROP TABLE IF EXISTS gstr.gstr_diag_inf_updated;
 
-CREATE TABLE gstr.gstr_diag_inf_updated AS
-    TABLE gstr.gstr_diag_inf
-    WITH NO DATA;
+CREATE TABLE gstr.gstr_diag_inf_updated AS TABLE gstr.gstr_diag_inf WITH NO DATA;
 
 ALTER TABLE gstr.gstr_diag_inf_updated  ADD COLUMN CREATED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_diag_inf_updated  ADD COLUMN CREATED_DATE TIMESTAMP;
 ALTER TABLE gstr.gstr_diag_inf_updated  ADD COLUMN LAST_MODIFIED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_diag_inf_updated  ADD COLUMN LAST_MODIFIED_DATE TIMESTAMP;
+---
 
--- 05
+ --- gstr_diag_stag
 DROP TABLE IF EXISTS gstr.gstr_diag_stag_updated;
 
-CREATE TABLE gstr.gstr_diag_stag_updated AS
-    TABLE gstr.gstr_diag_stag
-    WITH NO DATA;
+CREATE TABLE gstr.gstr_diag_stag_updated AS TABLE gstr.gstr_diag_stag WITH NO DATA;
 
 ALTER TABLE gstr.gstr_diag_stag_updated  ADD COLUMN CREATED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_diag_stag_updated  ADD COLUMN CREATED_DATE TIMESTAMP;
 ALTER TABLE gstr.gstr_diag_stag_updated  ADD COLUMN LAST_MODIFIED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.gstr_diag_stag_updated  ADD COLUMN LAST_MODIFIED_DATE TIMESTAMP;
+---
+
