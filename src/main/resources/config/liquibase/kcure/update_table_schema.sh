67b62209dcc5cae98d4b365b6ec0e5b2fd9ff274
zero <zero@planit.ai>
2022-11-16 16:35:23 +0900

    chore: update_table_schema - line wrap

--- src/main/resources/config/liquibase/kcure/update_table_schema.sh
+++ src/main/resources/config/liquibase/kcure/update_table_schema.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-array=( cncr_rgst gstr_pt_hlinf gstr_pt_fmht gstr_diag_aninf gstr_diag_inf gstr_diag_stag )
+array=(
+cncr_rgst
+gstr_pt_hlinf
+gstr_pt_fmht
+gstr_diag_aninf
+gstr_diag_inf
+gstr_diag_stag
+)
 
+cat /dev/null > meta/99_update_table_schema.sql
 for name in "${array[@]}"
 do
 DDLScript=$(cat <<EOF
-
-
--- ${name}
+ --- ${name}
 DROP TABLE IF EXISTS gstr.${name}_updated;
 
 CREATE TABLE gstr.${name}_updated AS TABLE gstr.${name} WITH NO DATA;
@@ -16,8 +22,10 @@ ALTER TABLE gstr.${name}_updated  ADD COLUMN CREATED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.${name}_updated  ADD COLUMN CREATED_DATE TIMESTAMP;
 ALTER TABLE gstr.${name}_updated  ADD COLUMN LAST_MODIFIED_BY CHARACTER VARYING(50);
 ALTER TABLE gstr.${name}_updated  ADD COLUMN LAST_MODIFIED_DATE TIMESTAMP;
+---\n
 )
 
 printf "$DDLScript"
-printf "$DDLScript" >> meta/99_update_table_schema.sql
+echo "$DDLScript" >> meta/99_update_table_schema.sql
 done
+
