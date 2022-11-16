425fe80ea03c6e2531a367fa6bc02e00496b30ae
zero <zero@planit.ai>
2022-11-16 16:02:51 +0900

    feat: update_table_schema.sh - add

---
+++ src/main/resources/config/liquibase/kcure/update_table_schema.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+array=( cncr_rgst gstr_pt_hlinf gstr_pt_fmht gstr_diag_aninf gstr_diag_inf gstr_diag_stag )
+
+for name in "${array[@]}"
+do
+DDLScript=$(cat <<EOF
+
+
+-- ${name}
+DROP TABLE IF EXISTS gstr.${name}_updated;
+
+CREATE TABLE gstr.${name}_updated AS TABLE gstr.${name} WITH NO DATA;
+
+ALTER TABLE gstr.${name}_updated  ADD COLUMN CREATED_BY CHARACTER VARYING(50);
+ALTER TABLE gstr.${name}_updated  ADD COLUMN CREATED_DATE TIMESTAMP;
+ALTER TABLE gstr.${name}_updated  ADD COLUMN LAST_MODIFIED_BY CHARACTER VARYING(50);
+ALTER TABLE gstr.${name}_updated  ADD COLUMN LAST_MODIFIED_DATE TIMESTAMP;
+)
+
+printf "$DDLScript"
+printf "$DDLScript" >> meta/99_update_table_schema.sql
+done
