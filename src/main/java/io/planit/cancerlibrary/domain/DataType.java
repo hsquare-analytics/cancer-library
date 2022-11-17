c8193b187b442b3f3807b826b92bfad03a929f19
zero <zero@planit.ai>
2022-11-17 10:11:40 +0900

    test: ItemResourceIT.java - fk err

---
+++ src/main/java/io/planit/cancerlibrary/domain/DataType.java
@@ -0,0 +1,60 @@
+package io.planit.cancerlibrary.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "ph_data_type")
+public class DataType implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Size(max = 50)
+    @Id
+    @Column(length = 50)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DataType name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof DataType)) {
+            return false;
+        }
+        return Objects.equals(name, ((DataType) o).name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "DataType{" +
+            "name='" + name + '\'' +
+            "}";
+    }
+}
