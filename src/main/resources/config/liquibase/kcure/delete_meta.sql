c5360f5b847987a0fd6f1a524f32873a6f548912
zero <zero@planit.ai>
2022-11-29 10:03:16 +0900

    chore: liquibase/kcure - delete meta sql

---
+++ src/main/resources/config/liquibase/kcure/delete_meta.sql
@@ -0,0 +1,9 @@
+delete from ph_codebook_lookup where 1 = 1;
+delete from ph_codebook where 1 = 1;
+delete from ph_item_attribute where 1 = 1;
+delete from ph_item_property where 1 = 1;
+delete from ph_item where 1 = 1;
+delete from ph_category_attribute where 1 = 1;
+delete from ph_group where 1 = 1;
+delete from ph_user_category where 1 = 1;
+delete from ph_category where 1 = 1;
