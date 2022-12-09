bb80738033dfc7c83a10defc8c81da63c05059d3
zero <zero@planit.ai>
2022-12-09 17:04:53 +0900

    Revert "feat: app.tsx - add theme cookie"
    This reverts commit f04d8f9e5fc136919f42b2f93f32f62e88d37425.

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -24,17 +24,6 @@ import {getDesignTokens} from 'app/shared/util/mui-theme';
 import {PaletteMode} from '@mui/material';
 import ToggleTheme from 'app/shared/layout/header/toggle-theme';
 import dxPopup from "devextreme/ui/popup";
-import {Cookies} from "react-cookie";
-
-const cookies = new Cookies();
-
-export const setCookie = (name: string, value: string, options: any) => {
-    cookies.set(name, value, options);
-}
-
-export const getCookie = (name: string) => {
-    return cookies.get(name);
-}
 
 dxPopup.defaultOptions({
   options: {animation: null}
@@ -68,7 +57,6 @@ export const App = () => {
   useEffect(() => {
     dispatch(getSession());
     dispatch(getProfile());
-    setMode(getCookie('theme') as PaletteMode);
   }, []);
 
   const onToggleMode = useCallback(
@@ -78,10 +66,8 @@ export const App = () => {
       const bodyClassList = document.body.classList;
 
       if (!bodyClassList.contains('dark')) {
-        setCookie('theme', 'dark', {path: baseHref});
         bodyClassList.add('dark');
       } else {
-        setCookie('theme', 'light', {path: baseHref});
         bodyClassList.remove('dark');
       }
     },
