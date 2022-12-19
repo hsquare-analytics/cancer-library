39d4afa1bfcb65d1a7c64a0e1ea06b4fc4c24bba
zero <zero@planit.ai>
2022-12-19 16:39:04 +0900

    modify: patient-profile-calendar.tsx - add layout

--- src/main/webapp/app/modules/datasource-editor/patient-profile/patient-profile-calendar.tsx
+++ src/main/webapp/app/modules/datasource-editor/patient-profile/patient-profile-calendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {convertDateFromServer} from "app/shared/util/date-utils";
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -8,29 +8,97 @@ import TextField from '@mui/material/TextField';
 import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 import IconButton from '@mui/material/IconButton';
 import {IPatient} from "app/shared/model/patient.model";
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import dayjs, {Dayjs} from 'dayjs';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {StaticDatePicker} from '@mui/x-date-pickers/StaticDatePicker';
+
 
 interface IPatientProfileDetailProps {
   patient: IPatient;
 }
+
 export const PatientProfileCalendar = (props: IPatientProfileDetailProps) => {
   const {patient} = props;
 
-  return <CardContent>
-    <Typography
-      color="text.secondary">{translate("cancerLibraryApp.patient.fsrMedDt")}
-      <IconButton onClick={() => {
-      }} style={{padding: '0', marginLeft: '3px'}}
-                  className="icon-patient-detail">
-        <CreateOutlinedIcon/>
-      </IconButton>
-    </Typography>
-    <Typography component="span" color="text.default">
-      <TextField
-        value={convertDateFromServer(patient['fsrMedDt'])}
-        variant={'standard'}
-        InputProps={{readOnly: true}}
-        className="text-field"
-      />
-    </Typography>
-  </CardContent>;
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-07'));
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(true);
+  };
+
+
+  useEffect(() => {
+    setValue(dayjs(patient['fsrMedDt']));
+  }, [patient]);
+
+
+  return <div>
+    <CardContent>
+      <Typography
+        color="text.secondary">{translate("cancerLibraryApp.patient.fsrMedDt")}
+        <IconButton onClick={() => setOpen(true)} style={{padding: '0', marginLeft: '3px'}}
+                    className="icon-patient-detail">
+          <CreateOutlinedIcon/>
+        </IconButton>
+      </Typography>
+      <Typography component="span" color="text.default">
+        <TextField
+          value={convertDateFromServer(patient['fsrMedDt'])}
+          variant={'standard'}
+          InputProps={{readOnly: true}}
+          className="text-field"
+        />
+      </Typography>
+    </CardContent>
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      aria-labelledby="responsive-dialog-title"
+      style={{
+        zIndex: 1502
+      }}
+      sx={{
+        '& .MuiDialogContent-root': {
+          padding: '0'
+        }
+      }}
+    >
+      <DialogTitle id="responsive-dialog-title">
+        {"초진일 변경"}
+      </DialogTitle>
+      <DialogContent>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <StaticDatePicker
+            displayStaticWrapperAs="desktop"
+            openTo="day"
+            value={value}
+            onChange={(newValue) => {
+              setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => {
+          setOpen(false)
+        }} autoFocus>
+          Save
+        </Button>
+        <Button autoFocus onClick={() => setOpen(false)}>
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </div>
+
 };
