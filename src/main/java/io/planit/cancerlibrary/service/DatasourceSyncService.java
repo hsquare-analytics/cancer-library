9c05821b49856b7ed8c4ad83df62c2ebd511728a
zero <zero@planit.ai>
2022-12-22 09:48:35 +0900

    modify: DatasourceSyncService - sync fdx

--- src/main/java/io/planit/cancerlibrary/service/DatasourceSyncService.java
+++ src/main/java/io/planit/cancerlibrary/service/DatasourceSyncService.java
@@ -1,13 +1,55 @@
 package io.planit.cancerlibrary.service;
 
+import io.planit.cancerlibrary.domain.Patient;
+import io.planit.cancerlibrary.repository.SqlExecutor;
+import org.apache.ibatis.jdbc.SQL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.util.MapUtils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class DatasourceSyncService {
 
-    public DatasourceSyncService() {
+    private final Logger log = LoggerFactory.getLogger(DatasourceSyncService.class);
+    private final SqlExecutor sqlExecutor;
+
+    public DatasourceSyncService(SqlExecutor sqlExecutor) {
+        this.sqlExecutor = sqlExecutor;
     }
 
-    public void syncFdx() {
+    public void syncPatientFdx(Patient patient) {
+        log.debug("Request to sync patient fdx : {}", patient);
+        String selectAllQuery = String.format("select * from gscn.cncr_rgst where pt_no = '%s'", patient.getPtNo());
+        List<Map<String, Object>> targetList = sqlExecutor.executeSelectAll(selectAllQuery);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        targetList.forEach(target -> {
+            LocalDateTime ldt = LocalDateTime.ofInstant(patient.getDetail().getStandardDate(), ZoneOffset.UTC);
+            target.put("fdx", ldt.format(formatter));
+            String getUpdatedDataQuery = String.format("select * from gscn.cncr_rgst_updated where idx = '%s'", target.get("idx"));
+            if (MapUtils.isEmpty(sqlExecutor.executeSelectOne(getUpdatedDataQuery))) {
+                SQL insertQuery = new SQL().INSERT_INTO("gscn.cncr_rgst_updated");
+                target.forEach((key, value) -> {
+                    if (value != null) {
+                        insertQuery.VALUES(key, String.format("'%s'", value));
+                    }
+                });
+                sqlExecutor.executeDML(insertQuery.toString());
+            } else {
+                String updateQuery = String.format("update gscn.cncr_rgst_updated set fdx = '%s' where idx = '%s'", patient.getDetail().getStandardDate(), target.get("idx"));
+                sqlExecutor.executeDML(updateQuery);
+            }
+        });
+
+
     }
 }
--- src/test/java/io/planit/cancerlibrary/service/DatasourceSyncServiceIT.java
+++ src/test/java/io/planit/cancerlibrary/service/DatasourceSyncServiceIT.java
@@ -1,10 +1,21 @@
 package io.planit.cancerlibrary.service;
 
 import io.planit.cancerlibrary.IntegrationTest;
+import io.planit.cancerlibrary.domain.Patient;
+import io.planit.cancerlibrary.domain.embedded.PatientDetail;
+import io.planit.cancerlibrary.repository.SqlExecutor;
+import io.planit.cancerlibrary.web.rest.PatientResourceIT;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 @IntegrationTest
 @Transactional
@@ -13,10 +24,26 @@ public class DatasourceSyncServiceIT {
     @Autowired
     private DatasourceSyncService datasourceSyncService;
 
+    @Autowired
+    private SqlExecutor sqlExecutor;
+
     @Test
     @Sql(scripts = "classpath:sql/cncr_rgst.ddl.sql")
     public void test_sync_fdx() {
-        // when:
-        datasourceSyncService.syncFdx();
+        // given
+        Patient patient = PatientResourceIT.createPatientDTO();
+        PatientDetail detail = patient.getDetail().standardDate(Instant.parse("2019-01-01T00:00:00Z"));
+
+        sqlExecutor.executeDML(String.format("insert into gscn.cncr_rgst (idx, hosp_cd, pt_no, crtn_dt, fdx) values ('1', '1234', '%s', '2020-01-01', '20200101')", patient.getPtNo()));
+
+        // when
+        datasourceSyncService.syncPatientFdx(patient.detail(detail));
+
+        // then
+        List<Map<String, Object>> result = sqlExecutor.executeSelectAll(String.format("select * from gscn.cncr_rgst_updated where pt_no = '%s'", patient.getPtNo()));
+
+        result.forEach(map -> {
+            assertThat(map.get("fdx")).isEqualTo("20190101");
+        });
     }
 }
