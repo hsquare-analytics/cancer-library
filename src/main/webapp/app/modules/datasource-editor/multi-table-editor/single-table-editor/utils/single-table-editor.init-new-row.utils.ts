f8d3024a4a6be5b7eedff6922b6e2e73db7e5d21
zero <zero@planit.ai>
2022-12-27 13:26:37 +0900

    modify: single-table-editor.init-new-row.utils.ts - init row

--- src/main/webapp/app/modules/datasource-editor/multi-table-editor/single-table-editor/utils/single-table-editor.init-new-row.utils.ts
+++ src/main/webapp/app/modules/datasource-editor/multi-table-editor/single-table-editor/utils/single-table-editor.init-new-row.utils.ts
@@ -1,6 +1,7 @@
 import {setCategory as setSelectedCategory} from "app/modules/datasource-editor/reducer/datasource.status.reducer";
 import {ActionType} from "app/modules/datasource-editor/multi-table-editor/single-table-editor/single-table-editor";
 import {ICategory} from "app/shared/model/category.model";
+import _ from "lodash";
 
 interface InitNewRowProps {
   e: any,
@@ -12,12 +13,17 @@ interface InitNewRowProps {
 
 export const SingleTableEditorOnInitNewRow = (props: InitNewRowProps) => {
   const {e, dispatch, dataGridRef, category} = props;
-  e.promise = new Promise<void>((resolve, reject) => {
-    const row = dataGridRef.current.instance.getVisibleRows().find(row => row.isSelected).data;
-    e.data = {...row};
 
-    dispatch(setSelectedCategory(category));
-    props.setActionType(ActionType.CREATE);
+  dispatch(setSelectedCategory(category));
+  props.setActionType(ActionType.CREATE);
+  const visibleRows = dataGridRef.current.instance.getVisibleRows().filter(row => row.isSelected).map(row => row.data);
+
+  e.promise = new Promise<void>((resolve, reject) => {
+    if (visibleRows.length > 0) {
+      const targetRows = visibleRows[0];
+      e.data = {...targetRows};
+    }
+
     resolve();
   });
 }
