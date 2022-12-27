26738bbe36a481f667080515984d03f2f575d2d3
zero <zero@planit.ai>
2022-12-27 16:00:58 +0900

    chore: accessible-patient.tsx - order no

--- src/main/webapp/app/modules/datasource-editor/accessible-patient.tsx
+++ src/main/webapp/app/modules/datasource-editor/accessible-patient.tsx
@@ -70,9 +70,9 @@ export const AccessiblePatient = () => {
 
   const getCalculateSortValue = (data: any) => {
     switch (data.detail.status) {
-      case  REVIEW_LIST.SUBMITTED:
-        return 1;
       case  REVIEW_LIST.DECLINED:
+        return 1;
+      case  REVIEW_LIST.SUBMITTED:
         return 2;
       case  REVIEW_LIST.APPROVED:
         return 3;
