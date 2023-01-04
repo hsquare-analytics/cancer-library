7203f06796c7cd2b947400ea79e311438efeb707
zero <zero@planit.ai>
2023-01-04 15:11:40 +0900

    feat: revert custom date range picker

--- src/main/webapp/app/modules/reviewer-statistics/reviewer-statistics.tsx
+++ src/main/webapp/app/modules/reviewer-statistics/reviewer-statistics.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import {DateRangePicker} from 'react-date-range';
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import React, {useState} from 'react';
+import DateRangePicker, {IDateRange} from "app/modules/date-range-picker";
+import moment from "moment";
 
 export interface IReviewStatisticsProps {
 }
 
 const ReviewerStatistics = (props: IReviewStatisticsProps) => {
-  const selectionRange = {
-    startDate: new Date(),
-    endDate: new Date(),
-    key: 'selection',
-  }
+  const [dateRange, setDateRange] = useState<IDateRange>({
+    startDate: moment().subtract(1, 'days').toDate(),
+    endDate: moment().subtract(1, 'days').toDate()
+  });
+
   return (
     <div className="reviewer-statistics-wrapper wrap-page">
       <h1 className="title-page">Reviewer Statistics</h1>
       <div>
-        <DateRangePicker
-          ranges={[selectionRange]}
-          onChange={() => {
-          }}
-        />
+        <DateRangePicker dateRange={dateRange} setDateRange={setDateRange}/>
+      </div>
+      <div>
+        {dateRange.startDate.toString()}
+      </div>
+      <div>
+        {dateRange.endDate.toString()}
       </div>
     </div>
   );
