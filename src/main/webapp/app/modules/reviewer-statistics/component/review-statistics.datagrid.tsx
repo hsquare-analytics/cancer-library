be343c00ca6662d85ae6643e380ccd19b53289d6
zero <zero@planit.ai>
2023-01-05 14:23:16 +0900

    modify: reviewer-statistics - loader

--- src/main/webapp/app/modules/reviewer-statistics/component/review-statistics.datagrid.tsx
+++ src/main/webapp/app/modules/reviewer-statistics/component/review-statistics.datagrid.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 
-import DataGrid, {
-  Column,
-  Grouping,
-  GroupPanel,
-  Pager,
-  Paging,
-  SearchPanel,
-} from 'devextreme-react/data-grid';
+import DataGrid, {Column, Paging, SearchPanel,} from 'devextreme-react/data-grid';
 import {useAppSelector} from "app/config/store";
+import {translate} from "react-jhipster";
+
 
 const ReviewStatisticsDatagrid = () => {
   const entities = useAppSelector(state => state.reviewerStatistics.entities);
@@ -18,24 +13,30 @@ const ReviewStatisticsDatagrid = () => {
       allowColumnReordering={true}
       rowAlternationEnabled={true}
       showBorders={true}
+      searchPanel={{visible: false}}
+      allowColumnResizing={true}
     >
       <SearchPanel visible={true} highlightCaseSensitive={true}/>
-      <Column dataField="id" dataType="text"/>
-      <Column dataField="name" dataType="text"/>
-      <Column dataField="total_patient" dataType="string"/>
+      <Column dataField="login" dataType="text" alignment="center"/>
+      <Column dataField="name" dataType="text" alignment="center"/>
+      <Column dataField="assigned" dataType="string" alignment="center"/>
+      <Column
+        caption={translate('cancerLibraryApp.userPatientDndGrid.column.totalSubmitted')}
+        calculateCellValue={rowData => rowData.submitted + rowData.approved + rowData.declined}
+        alignment="center"/>
       <Column caption={"count"}>
-        <Column dataField="noaction" dataType="string"/>
-        <Column dataField="declined" dataType="string"/>
-        <Column dataField="approved" dataType="string"/>
+        <Column dataField="submitted" dataType="string" alignment="center"/>
+        <Column dataField="declined" dataType="string" alignment="center"/>
+        <Column dataField="approved" dataType="string" alignment="center"/>
       </Column>
       <Column caption={"%"}>
-        <Column dataField="noaction" dataType="string"/>
-        <Column dataField="declined" dataType="string"/>
-        <Column dataField="approved" dataType="string"/>
+        <Column dataField="submitted" dataType="string" alignment="center"/>
+        <Column dataField="declined" dataType="string" alignment="center"/>
+        <Column dataField="approved" dataType="string" alignment="center"/>
       </Column>
       <Column caption={"% per total patient"}>
-        <Column dataField="noaction" dataType="string"/>
-        <Column dataField="declined" dataType="string"/>
+        <Column dataField="noaction" dataType="string" alignment="center"/>
+        <Column dataField="declined" dataType="string" alignment="center"/>
       </Column>
       <Paging defaultPageSize={10}/>
     </DataGrid>);
