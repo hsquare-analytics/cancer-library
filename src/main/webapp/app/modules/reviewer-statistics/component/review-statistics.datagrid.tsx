4f3bec0ee18a29f2bd0b848caa81b099440cee00
zero <zero@planit.ai>
2023-01-05 14:56:33 +0900

    modify: review-statistics.datagrid.tsx - label

--- src/main/webapp/app/modules/reviewer-statistics/component/review-statistics.datagrid.tsx
+++ src/main/webapp/app/modules/reviewer-statistics/component/review-statistics.datagrid.tsx
@@ -7,6 +7,17 @@ import {translate} from "react-jhipster";
 
 const ReviewStatisticsDatagrid = () => {
   const entities = useAppSelector(state => state.reviewerStatistics.entities);
+
+  const getRatioOfField = (rowData: any, field: string) => {
+    const parent = rowData.submitted + rowData.approved + rowData.declined;
+
+    if (parent === 0) {
+      return '-';
+    }
+
+    return (rowData[field] / parent) * 100 + '%'
+  }
+
   return (
     <DataGrid
       dataSource={entities}
@@ -21,30 +32,33 @@ const ReviewStatisticsDatagrid = () => {
               caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.login')}/>
       <Column dataField="name" dataType="text" alignment="center"
               caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.name')}/>
-      <Column dataField="assigned" dataType="string" alignment="center"
-              caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.assigned')}/>
       <Column calculateCellValue={rowData => rowData.submitted + rowData.approved + rowData.declined}
               caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.total')}
               alignment="center"/>
-      <Column alignment="center" caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.count.title')}>
+      <Column alignment="center" caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.status.title')}>
         <Column dataField="submitted" dataType="string" alignment="center"
-                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.count.submitted')}/>
-        <Column dataField="declined" dataType="string" alignment="center"
-                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.count.declined')}/>
+                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.status.submitted')}/>
         <Column dataField="approved" dataType="string" alignment="center"
-                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.count.approved')}/>
+                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.status.approved')}/>
+        <Column dataField="declined" dataType="string" alignment="center"
+                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.status.declined')}/>
       </Column>
       <Column alignment="center"
               caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.percent.title')}>
         <Column dataField="submitted" dataType="string" alignment="center"
+                calculateCellValue={rowData => getRatioOfField(rowData, 'submitted')}
                 caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.percent.submitted')}/>
-        <Column dataField="declined" dataType="string" alignment="center"
-                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.percent.declined')}/>
         <Column dataField="approved" dataType="string" alignment="center"
+                calculateCellValue={rowData => getRatioOfField(rowData, 'approved')}
                 caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.percent.approved')}/>
+        <Column dataField="declined" dataType="string" alignment="center"
+                calculateCellValue={rowData => getRatioOfField(rowData, 'declined')}
+                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.percent.declined')}/>
       </Column>
       <Column alignment="center"
               caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.totalRatio.title')}>
+        <Column dataField="assigned" dataType="string" alignment="center"
+                caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.assigned')}/>
         <Column dataField="submitted" dataType="string" alignment="center"
                 caption={translate('cancerLibraryApp.reviewerStatistics.dataGrid.totalRatio.submitted')}/>
         <Column dataField="approved" dataType="string" alignment="center"
@@ -52,6 +66,5 @@ const ReviewStatisticsDatagrid = () => {
       </Column>
       <Paging defaultPageSize={10}/>
     </DataGrid>);
-};
-
+}
 export default ReviewStatisticsDatagrid;
