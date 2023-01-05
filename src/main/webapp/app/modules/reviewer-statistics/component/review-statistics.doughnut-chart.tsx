aef8926f9185ba1251a970d74eada161a17e88eb
zero <zero@planit.ai>
2023-01-05 15:59:31 +0900

    modify: review-statistics.doughnut-chart.tsx - chart

--- src/main/webapp/app/modules/reviewer-statistics/component/review-statistics.doughnut-chart.tsx
+++ src/main/webapp/app/modules/reviewer-statistics/component/review-statistics.doughnut-chart.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
 import {Doughnut} from 'react-chartjs-2';
+import {Pie} from 'react-chartjs-2';
+import {useAppSelector} from "app/config/store";
+import {translate} from "react-jhipster";
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -32,7 +36,59 @@ export const data = {
 };
 
 const ReviewStatisticsDoughnutChart = () => {
-  return <Doughnut data={data}/>;
+  const [labels, setLabels] = useState<string[]>([]);
+  const [datasets, setDatasets] = useState<any[]>([]);
+
+  const entities = useAppSelector(state => state.reviewerStatistics.entities);
+
+  useEffect(() => {
+    if (entities.length > 0) {
+      const labels = entities.map(entity => `${entity.name}(${entity.login})`);
+      setLabels(labels);
+
+      const totalCount = entities.map(entity => entity.submitted + entity.approved + entity.declined).reduce((a, b) => a + b, 0);
+
+      const temp = [
+        {
+          // label: `${totalCount} #`,
+          data: [],
+          backgroundColor: [],
+          borderColor: [ ],
+          borderWidth: 1,
+        },
+      ];
+
+      const RandomColor = () => {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return {
+          backgroundColor: `rgba(${r}, ${g}, ${b}, 0.2)`,
+          borderColor: `rgba(${r}, ${g}, ${b}, 0.8)`,
+        }
+      }
+
+      const data = entities.map(entity => entity.submitted + entity.approved + entity.declined);
+      const backgroundColor = [];
+      const borderColor = [];
+      entities.forEach((entity, index) => {
+        const color = RandomColor();
+        backgroundColor.push(color.backgroundColor);
+        borderColor.push(color.borderColor);
+      });
+
+      temp[0].data = data;
+      temp[0].backgroundColor = backgroundColor;
+      temp[0].borderColor = borderColor;
+      setDatasets(temp);
+    }
+
+  }, [JSON.stringify(entities)]);
+
+  return <Pie data={{
+    labels: labels,
+    datasets: datasets
+  }}/>;
 }
 
 export default ReviewStatisticsDoughnutChart;
