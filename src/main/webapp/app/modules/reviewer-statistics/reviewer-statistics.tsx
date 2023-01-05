9afd1ccfd1a98552b86137302a5c115c4b1bce46
zero <zero@planit.ai>
2023-01-05 09:14:24 +0900

    modify: reviewer-statistics.tsx - layout

--- src/main/webapp/app/modules/reviewer-statistics/reviewer-statistics.tsx
+++ src/main/webapp/app/modules/reviewer-statistics/reviewer-statistics.tsx
@@ -1,62 +1,8 @@
 import React, {useState} from 'react';
 import DateRangePicker, {IDateRange} from "app/modules/date-range-picker";
 import moment from "moment";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: false,
-    },
-  },
-};
-
-const labels = ['kcure01', 'kcure02', 'kcure03', 'kcure04', 'kcure05'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'submit ',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(255, 205, 86, 0.5)',
-    },
-    {
-      label: 'decline',
-      data: [ 10, 20, 30, 40, 50 ],
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'approve',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    }
-  ],
-};
-
+import BarChart from "app/modules/reviewer-statistics/bar-chart";
+import "./reviewer-statistics.scss";
 
 export interface IReviewStatisticsProps {
 }
@@ -73,8 +19,8 @@ const ReviewerStatistics = (props: IReviewStatisticsProps) => {
       <div className="m-2 mt-4 d-flex justify-content-end">
         <DateRangePicker dateRange={dateRange} setDateRange={setDateRange}/>
       </div>
-      <div>
-        <Bar options={options} data={data} />
+      <div className="left-section">
+        <BarChart/>
       </div>
     </div>
   );
