36fd45bc3ac04e0e2972325af6e1a91a8f646a8d
zero <zero@planit.ai>
2023-01-09 16:01:29 +0900

    fix: AuditResource - transaction

--- src/main/java/io/planit/cancerlibrary/web/rest/AuditResource.java
+++ src/main/java/io/planit/cancerlibrary/web/rest/AuditResource.java
@@ -5,6 +5,7 @@ import io.planit.cancerlibrary.repository.PersistenceAuditEventRepository;
 import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/management/audits")
+@Transactional
 public class AuditResource {
 
     private final PersistenceAuditEventRepository persistenceAuditEventRepository;
