05a10bafc8c54923772ca3913a9ad7a020790529
zero <zero@planit.ai>
2023-01-11 14:23:28 +0900

    feat: theme setting with cookie

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -26,6 +26,8 @@ import ToggleTheme from 'app/shared/layout/header/toggle-theme';
 import dxPopup from "devextreme/ui/popup";
 import "@fontsource/roboto"; // Defaults to weight 400.
 import {ArcElement, BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip} from 'chart.js';
+import {useCookies} from 'react-cookie';
+
 
 ChartJS.register(
   CategoryScale,
@@ -38,14 +40,16 @@ ChartJS.register(
 );
 
 dxPopup.defaultOptions({
-  options: {animation: {
+  options: {
+    animation: {
       show: {type: "fade", duration: 400, from: 0, to: 1},
       hide: {type: "fade", duration: 400, from: 1, to: 0}
       // show: {type: "slide", duration: 400, from: {position: {my: "top", at: "bottom", of: window}}},
       // hide: {type: "slide", duration: 400, to: {position: {my: "top", at: "bottom", of: window}}}
       // show: {type: "pop", duration: 400, from: {scale: 0}},
       // hide: {type: "pop", duration: 400, to: {scale: 0}}
-    }}
+    }
+  }
 });
 
 const baseHref = document.querySelector('base').getAttribute('href').replace(/\/$/, '');
@@ -66,10 +70,12 @@ declare module '@mui/material/styles' {
 }
 
 export const App = () => {
+  const [cookies, setCookie] = useCookies(['theme']);
+
   const dispatch = useAppDispatch();
 
   // todo: 유저가 테마를 설정 한 이력이 있는경우 저장된 테마 정보를 불러오게 한다
-  const [mode, setMode] = React.useState<PaletteMode>('light');
+  const [mode, setMode] = React.useState<PaletteMode>(cookies.theme ? cookies.theme : 'light');
 
   const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
@@ -78,20 +84,20 @@ export const App = () => {
     dispatch(getProfile());
   }, []);
 
-  const onToggleMode = useCallback(
-    (value: PaletteMode) => {
-      setMode(value);
+  useEffect(() => {
+    setMode(cookies.theme);
+    if (cookies.theme === 'dark') {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+    } else {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+    }
+  }, [cookies.theme]);
 
-      const bodyClassList = document.body.classList;
-
-      if (!bodyClassList.contains('dark')) {
-        bodyClassList.add('dark');
-      } else {
-        bodyClassList.remove('dark');
-      }
-    },
-    [mode]
-  );
+  const onToggleMode = useCallback((value: PaletteMode) => {
+    setCookie('theme', value, {path: '/'});
+  }, [mode]);
 
   const currentLocale = useAppSelector(state => state.locale.currentLocale);
   const isAuthenticated = useAppSelector(state => state.authentication.isAuthenticated);
@@ -112,7 +118,7 @@ export const App = () => {
           <ToastContainer
             position={toast.POSITION.TOP_RIGHT}
             className="toastify-container"
-            theme="light"
+            theme={mode}
             toastClassName="toastify-toast"
             limit={5}
           />
